e3fc7419ee79d831599f59632bddbd8645b19f7a
Renju Radhakrishnan <renju467@gmail.com>
2016-06-28 17:19:58 -0400

    kernel source code

---
+++ linux-2.6.22.19-cs543/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 Tue Jun 28 15:34:53 EDT 2016"
+#define LINUX_COMPILE_TIME "15:34:53"
+#define LINUX_COMPILE_BY "rr553"
+#define LINUX_COMPILE_HOST "tux64-14.cs.drexel.edu"
+#define LINUX_COMPILE_DOMAIN "cs.drexel.edu"
+#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04.1) "
