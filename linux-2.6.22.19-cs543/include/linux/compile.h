479599904cde74103e3bc9006720fb8bad6cd160
Renju Radhakrishnan <renju467@gmail.com>
2016-08-28 12:13:40 -0400

    removed linux src files

--- linux-2.6.22.19-cs543/include/linux/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 4 */
-#define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#4 Wed Jul 13 19:29:35 EDT 2016"
-#define LINUX_COMPILE_TIME "19:29:35"
-#define LINUX_COMPILE_BY "rr553"
-#define LINUX_COMPILE_HOST "tux64-13.cs.drexel.edu"
-#define LINUX_COMPILE_DOMAIN "cs.drexel.edu"
-#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04.3) "
